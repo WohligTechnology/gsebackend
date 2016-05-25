b2cf4e155608264e21385e52773f5aa3127eb10f
Pooja <pthakare33@gmail.com>
2016-05-25 12:27:03 +0530

    image sizes

--- application/views/backend/edithighlight.php
+++ application/views/backend/edithighlight.php
@@ -33,6 +33,8 @@
 					<input class="file-path validate" type="text" placeholder="Upload one or more files" value="<?php echo set_value('image',$before->image);?>">
 				</div>
 			</div>
+			<span style=" display: block;
+	 padding-top: 30px;">630 X 285</span>
 		</div>
 <div class="row">
 <div class="input-field col s6">
