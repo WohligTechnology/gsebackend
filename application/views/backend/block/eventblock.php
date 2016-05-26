6b3d9f4d173c2c4487ca1ab04cfe40d3c3245c04
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-05-26 18:26:01 +0530

    chnages

---
+++ application/views/backend/block/eventblock.php
@@ -0,0 +1,20 @@
+<section class="panel">
+    <div class="panel-body">
+        <ul id="nav-mobile">
+            <li><a class="waves-effect waves-light <?php if ($this->uri->segment(2) == 'editGallery') {
+    echo 'active';
+} ?>" href="<?php echo site_url('site/editwedding?id=').$before1; ?>">Go to wedding</a></li>
+            <li><a class="waves-effect waves-light <?php if ($this->uri->segment(2) == 'viewGalleryImage' || $this->uri->segment(2) == 'editGalleryImage'  || $this->uri->segment(2) == 'createGalleryImage') {
+    echo 'active';
+} ?>" href="<?php echo site_url('site/viewweddingtype?id=').$before2; ?>"> Videos</a></li> 
+      <li><a class="waves-effect waves-light <?php if ($this->uri->segment(2) == 'viewGalleryImage' || $this->uri->segment(2) == 'editGalleryImage'  || $this->uri->segment(2) == 'createGalleryImage') {
+    echo 'active';
+} ?>" href="<?php echo site_url('site/viewweddingsubtype?id=').$before3; ?>">Sub-Type</a></li>  
+   <li><a class="waves-effect waves-light <?php if ($this->uri->segment(2) == 'viewGalleryImage' || $this->uri->segment(2) == 'editGalleryImage'  || $this->uri->segment(2) == 'createGalleryImage') {
+    echo 'active';
+} ?>" href="<?php echo site_url('site/viewweddinggallery?id=').$before4; ?>">Gallery</a></li> 
+         
+ 
+        </ul>
+    </div>
+</section>
