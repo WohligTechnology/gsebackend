2b4d3d53063f218d4d9567b551bdb33fcf2be9c9
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-06-22 18:52:03 +0530

    changes

--- application/views/backend/viewservice.php
+++ application/views/backend/viewservice.php
@@ -30,6 +30,19 @@ function drawtable(resultrow) {
     else if(resultrow.type==2){
         resultrow.type="Sports";
     }
+    else if(resultrow.type==3){
+        resultrow.type="Events";
+    }
+    else if(resultrow.type==4){
+        resultrow.type="Talents";
+    }
+    else if(resultrow.type==5){
+        resultrow.type="Mice";
+    }
+    else if(resultrow.type==6){
+        resultrow.type="World Tour";
+    }
+
 return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.name + "</td><td>" + resultrow.content + "</td><td>" + resultrow.order + "</td><td>" + resultrow.type + "</td><td><a class='btn btn-primary btn-xs waves-effect waves-light blue darken-4 z-depth-0 less-pad' href='<?php echo site_url('site/editservice?id=');?>"+resultrow.id+"' data-position='top' data-delay='50' data-tooltip='Edit'><i class='fa fa-pencil propericon'></i></a><a class='btn btn-danger btn-xs waves-effect waves-light red pad10 z-depth-0 less-pad' onclick=\"return confirm('Are you sure you want to delete?');\") href='<?php echo site_url('site/deleteservice?id='); ?>"+resultrow.id+"' data-position='top' data-delay='50' data-tooltip='Delete'><i class='material-icons propericon'>delete</i></a></td></tr>";
 }
 generatejquery("<?php echo $base_url;?>");
